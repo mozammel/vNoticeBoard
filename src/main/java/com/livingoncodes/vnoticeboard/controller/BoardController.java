816945a282d5dbf0169b5a760339dd53f18d9ce4
Mozammel Haque <mozammel@livingoncodes.com>
2015-01-16 09:09:59 +0600

    Show board data

--- src/main/java/com/livingoncodes/vnoticeboard/controller/BoardController.java
+++ src/main/java/com/livingoncodes/vnoticeboard/controller/BoardController.java
@@ -2,23 +2,42 @@ package com.livingoncodes.vnoticeboard.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.livingoncodes.vnoticeboard.entity.Board;
+import com.livingoncodes.vnoticeboard.repository.BoardRepository;
+
 @Controller
 public class BoardController {
-
+	
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(BoardController.class);
+	
+	@Autowired
+	private BoardRepository boardRepository;
 	
 	@RequestMapping(value ="/{boardName}", method = RequestMethod.GET)
 	public String showBoard(@PathVariable("boardName") String boardName, Model model) {
 		LOGGER.debug("Rendering board page: " + boardName);
 		
+		Board board = boardRepository.findByName(boardName);
+		
+		if( board == null) {
+			LOGGER.debug("Board not found: " + boardName);
+			
+			// Add the not found boardname to model so that we can use it
+			// in the new form
+			model.addAttribute("boardName", boardName);
+			return "newboard";
+		}
+		
 		model.addAttribute("boardName", boardName);
+		model.addAttribute("boardContent", board.getContent());
 		return "board";
 	}
 
--- src/test/java/com/livingoncodes/vnoticeboard/repository/BoardRepositoryTest.java
+++ src/test/java/com/livingoncodes/vnoticeboard/repository/BoardRepositoryTest.java
@@ -22,11 +22,19 @@ public class BoardRepositoryTest {
 	@Test
 	public void findByNameTest() {
 		
-		Board board1 = new Board(1L, "tinytots", "testpass", "Sample board text");
+		Board board1 = new Board(1L, "tinytots", "testpass1", "Sample board text1");
+		Board board2 = new Board(2L, "stjoseph", "testpass2", "Sample board text2");
+		Board board3 = new Board(3L, "rifelspublic", "testpass3", "Sample board text3");
+		Board board4 = new Board(4L, "govtboys", "testpass4", "Sample board text4");
+		Board board5 = new Board(5L, "shaheen", "testpass5", "Sample board text5");
 
 		boardRepository.save(board1);
+		boardRepository.save(board2);
+		boardRepository.save(board3);
+		boardRepository.save(board4);
+		boardRepository.save(board5);
 		
-		System.out.println(boardRepository.findByName("tinytots"));
+		System.out.println(boardRepository.findByName("govtboys"));
 
 		
 	}
