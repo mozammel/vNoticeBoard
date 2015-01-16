f9df1ac5a45ab21480750c64e8a1f07fef7e0959
Mozammel Haque <mozammel@livingoncodes.com>
2015-01-16 13:00:37 +0600

    new board form

--- src/main/java/com/livingoncodes/vnoticeboard/controller/BoardController.java
+++ src/main/java/com/livingoncodes/vnoticeboard/controller/BoardController.java
@@ -1,14 +1,20 @@
 package com.livingoncodes.vnoticeboard.controller;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.livingoncodes.vnoticeboard.dto.RegistrationForm;
 import com.livingoncodes.vnoticeboard.entity.Board;
 import com.livingoncodes.vnoticeboard.repository.BoardRepository;
 
@@ -20,6 +26,29 @@ public class BoardController {
 	
 	@Autowired
 	private BoardRepository boardRepository;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerBoard(@Valid RegistrationForm registrationForm,
+            BindingResult result, WebRequest request, RedirectAttributes redirectAttributes) {
+		
+		System.out.println(request.getParameter("boardName"));
+		System.out.println(request.getParameter("password"));
+		
+		if( result.hasErrors() ) {
+			System.out.println(result.getAllErrors());
+			redirectAttributes.addAttribute("error", "Password should be more than 3 characters and less than 100");
+			return "redirect:/" + request.getParameter("boardName");
+		}
+
+		Board board = new Board();
+		
+		board.setName(request.getParameter("boardName"));
+		board.setPassword(request.getParameter("password"));
+		
+		boardRepository.save(board);
+		return "redirect:/" + request.getParameter("boardName");
+	}
+	
 	
 	@RequestMapping(value ="/{boardName}", method = RequestMethod.GET)
 	public String showBoard(@PathVariable("boardName") String boardName, Model model) {
